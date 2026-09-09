635d26c7e08d6a9212e69ae8b29c38b57c64ef01
yohan131313 <skadygks7752@naver.com>
2026-09-09 13:50:13 +0900

    week2

---
+++ lab/week2/happy.py
@@ -0,0 +1,12 @@
+def congratulate(name):
+    print("안녕하세요?")
+    print(name + "님, 생일 축하합니다!")
+    return None
+
+def test_congratulate():
+    name_list = ["요한", "민", "필구", "현준"]
+    for name in name_list:
+        congratulate(name)
+
+if __name__ == "__main__":
+    test_congratulate()
